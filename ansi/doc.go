0536c543efe1315d216f0935854ed0d155c7a2ed
Joshua T Corbin <jcorbin@wunjo.org>
2018-08-09 22:10:53 -0700

    ansi: import ansicode.txt, translate to ansicode.md

---
+++ ansi/doc.go
@@ -0,0 +1,10 @@
+/*Package ansi provides support for text encoded ANSI, focusing especially on
+escape and control sequences.
+
+Primary focus is on bridging the ANSI world into the Unicode/UTF-8 world.
+
+ISO-2022 is currently supported minimally and assumed to be in UTF-8 mode at
+all times via the "ESC % G" sequence.
+
+*/
+package ansi
