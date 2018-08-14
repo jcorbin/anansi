50bd18fa77e50e66e05f5d2a378c6a3215e2e314
Joshua T Corbin <jcorbin@wunjo.org>
2018-08-14 00:00:49 -0700

    ansi: add decode example test

---
+++ ansi/decode_example_test.go
@@ -0,0 +1,101 @@
+package ansi_test
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"unicode/utf8"
+
+	"github.com/jcorbin/anansi/ansi"
+)
+
+func Example_decode_main() {
+	switch err := run(); err {
+	case nil:
+	case io.EOF:
+		fmt.Println(err)
+	default:
+		log.Fatal(err)
+	}
+}
+
+func run() error {
+	const minRead = 128
+	var buf bytes.Buffer
+	for {
+		// read more input...
+		buf.Grow(minRead)
+		p := buf.Bytes()
+		p = p[len(p):cap(p)]
+		n, err := os.Stdin.Read(p)
+		if err != nil {
+			panic(err)
+		}
+		if n == 0 {
+			continue
+		}
+		_, _ = buf.Write(p[:n])
+
+		// ...and process it
+	process:
+		for buf.Len() > 0 {
+			e, a, n := ansi.DecodeEscape(buf.Bytes())
+			if n > 0 {
+				buf.Next(n)
+			}
+
+			// have a complete escape sequence
+			if e != 0 {
+				handleEscape(e, a)
+				continue
+			}
+
+			// try to decode a rune, maybe read more bytes to complete a
+			// partial escape sequence
+			switch r, n := utf8.DecodeRune(buf.Bytes()); r {
+			case 0x90, 0x9B, 0x9D, 0x9E, 0x9F: // DCS, CSI, OSC, PM, APC
+				break process
+			case 0x1B: // ESC
+				if p := buf.Bytes(); len(p) == cap(p) {
+					break process
+				}
+				fallthrough
+			default:
+				buf.Next(n)
+				handleRune(r)
+			}
+		}
+	}
+}
+
+func process() error {
+	return nil
+}
+
+func handleEscape(e ansi.Escape, a []byte) {
+	fmt.Print(e)
+	if len(a) > 0 {
+		fmt.Printf(" %q", a)
+	}
+	fmt.Printf("\r\n")
+}
+
+func handleRune(r rune) {
+	switch {
+	// print C0 controls phonetically
+	case r < 0x20, r == 0x7f:
+		fmt.Printf("^%s", string(0x40^r))
+
+	// print C1 controls mnemonically
+	case 0x80 <= r && r <= 0x9f:
+		fmt.Print(ansi.C1Names[r&0x1f])
+
+	// print normal rune
+	default:
+		fmt.Print(string(r))
+	}
+
+}
+
