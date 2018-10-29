4c5c4a314060e8d3b38036803eae940ecd5e8260
Joshua T Corbin <jcorbin@wunjo.org>
2018-10-29 11:23:42 -0700

    anansi/x/platform: collect and log rusage telemetry

--- x/platform/platform.go
+++ x/platform/platform.go
@@ -67,6 +67,7 @@ func New(opts ...Option) (*Platform, error) {
 	p.FPSEstimate.data = make([]float64, defaultFrameRate)
 	p.Timing.ts = make([]time.Time, timingPeriod)
 	p.Timing.ds = make([]time.Duration, timingPeriod)
+	p.Telemetry.coll.rusage.data = make([]rusageEntry, defaultFrameRate*10)
 	p.bgworkers = append(p.bgworkers, &p.Telemetry.coll, &Logs)
 
 	if !hasConfig(opts) {
