0822753349e486725c7cace7f76088d3988d416a
Joshua T Corbin <jcorbin@wunjo.org>
2018-11-06 09:02:40 -0800

    cmd/decode: ex ansi example

---
+++ cmd/decode/main.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"unicode/utf8"
+
+	"github.com/jcorbin/anansi/ansi"
+)
+
+func main() {
+	switch err := run(); err {
+	case nil:
+	case io.EOF:
+		fmt.Println(err)
+	default:
+		log.Fatal(err)
+	}
+}
+
+func run() error {
+	const minRead = 128
+	var buf bytes.Buffer
+	for {
+		// read more input...
+		buf.Grow(minRead)
+		p := buf.Bytes()
+		p = p[len(p):cap(p)]
+		n, err := os.Stdin.Read(p)
+		if err != nil {
+			return err
+		}
+		if n == 0 {
+			continue
+		}
+		_, _ = buf.Write(p[:n])
+
+		// ...and process it
+		if err := process(&buf); err != nil {
+			return err
+		}
+	}
+}
+
+func process(buf *bytes.Buffer) error {
+	for buf.Len() > 0 {
+		e, a, n := ansi.DecodeEscape(buf.Bytes())
+		if n > 0 {
+			buf.Next(n)
+		}
+
+		// have a complete escape sequence
+		if e != 0 {
+			handleEscape(e, a)
+			continue
+		}
+
+		// Decode a rune...
+		switch r, n := utf8.DecodeRune(buf.Bytes()); r {
+
+		case 0x90, 0x9D, 0x9E, 0x9F: // DCS, OSC, PM, APC
+			return nil // ... need more bytes to complete a partial string.
+
+		case 0x9B: // CSI
+			return nil // ... need more bytes to complete a partial control sequence.
+
+		case 0x1B: // ESC
+			if p := buf.Bytes(); len(p) == cap(p) {
+				return nil // ... need more bytes to determine if an escape sequence can be decoded.
+			}
+			fallthrough // ... literal ESC
+
+		default:
+			buf.Next(n)
+			handleRune(r)
+		}
+	}
+	return nil
+}
+
+func handleEscape(e ansi.Escape, a []byte) {
+	fmt.Print(e)
+	if len(a) > 0 {
+		fmt.Printf(" %q", a)
+	}
+	fmt.Printf("\r\n")
+}
+
+func handleRune(r rune) {
+	switch {
+	// print C0 controls phonetically
+	case r < 0x20, r == 0x7f:
+		fmt.Printf("^%s", string(0x40^r))
+
+	// print C1 controls mnemonically
+	case 0x80 <= r && r <= 0x9f:
+		fmt.Print(ansi.C1Names[r&0x1f])
+
+	// print normal rune
+	default:
+		fmt.Print(string(r))
+	}
+}
